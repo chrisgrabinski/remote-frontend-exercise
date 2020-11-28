afdf864154b88124b6947aad2a323c882883e43a
Chris Grabiński <hello@chrisgrabinski.com>
2020-11-28 17:43:08 +0000

    fixed styled components implementation, added global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import Head from "next/head";
+import { createGlobalStyle } from "styled-components";
 import "tailwindcss/dist/base.css";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #F4F7FC;
+    color: #778CA3;
+    font-family: 'Inter', sans-serif;
+  }
+`;
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
