21395db45cb98e725684d382036e806376d6e25a
Chris Grabiński <hello@chrisgrabinski.com>
2020-11-29 19:52:39 +0000

    adjusted test description

--- utils/format-date.test.js
+++ utils/format-date.test.js
@@ -1,6 +1,6 @@
 import formatDate from "./format-date";
 
-test("format a date to the DD/MM/YYYY format", () => {
+test("formats a date string to DD/MM/YYYY", () => {
   expect(formatDate("1987-08-11")).toEqual("11/08/1987");
   expect(formatDate).toThrow();
   expect(() => formatDate("This is a string")).toThrow();
